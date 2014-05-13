684b126995f29473c69680ca5f19b4ad5be3fec2
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-13 23:56:18 +1200

    Create NameSpace1.php

---
+++ NameSpace1.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MyNameSpace\TestClass;
+
+  use \DateTime;
+// (or)
+//  use DateTime;
+
+class myclass{
+
+	public $x=420;
+	
+	public function Func(){
+		
+		//$dt = new \DateTime();
+		$dt = new DateTime();		
+		
+		return $dt;
+	}
+}
+
+?>
