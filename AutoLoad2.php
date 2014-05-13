7139b8ed17cc429aa50f85771267fa276281bb6a
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-13 15:25:58 +1200

    Create AutoLoad2.php

---
+++ AutoLoad2.php
@@ -0,0 +1,16 @@
+<?php
+ 
+/* You can use this if all your files are located in a single directory */
+
+set_include_path(get_include_path().PATH_SEPARATOR.realpath('path/to/class'));
+ 
+function autoload($className)
+{
+    include $className.'.php';
+}
+ 
+spl_autoload_register('autoload');
+ 
+$myClass = new myclass();
+ 
+?>
