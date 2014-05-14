14a9b6fcfffd34f74cafd4b790fd3eeda4b51b60
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-14 14:32:57 +1200

    Update Construct.php

--- Construct.php
+++ Construct.php
@@ -2,17 +2,32 @@
 
 /* Each time object created newly, call constructor that is why it is suitable for any initialization before it is used. */
 
-class myclass{
 /*
+class myclass{
+
 	public function __construct(){ // Constructor
 		echo "I am constructor";
 	}
-*/
+
 	public function myclass(){ // Constructor
 		echo "I am myclass";
 	}	
 }
 
 $Objmyclass =  new myclass();
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace mynamespace;
+
+class myclass{
+
+	public function myclass(){ // Not a Constructor act like regular fucntion in namepasce class
+		echo "I am constructor";
+	}
+}
+
+$Objmyclass =  new myclass(); // Nothing print
 
 ?>
