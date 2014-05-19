3029b8e7efccb25d13e7f837771db196f7f2dc0c
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-20 05:10:39 +1200

    Update PubPriPro.php

--- PubPriPro.php
+++ PubPriPro.php
@@ -1,11 +1,28 @@
 <?php
 
+/* visibility of a property or method can be declared with the keywords public, protected or private. */
+
+/* 
+* Class members declared public can be accessed everywhere.
+*  Members declared protected can be accessed only within the class itself and by inherited and parent classes.
+* Members declared as private may only be accessed by the class that defines the member. 
+*/
+
+
 class myclass{
 
+/* Class properties must be defined as public, private, or protected. If declared using var, the property will be defined as public.  */
+
+	// var $pub=100;
+	
 	public $pub=100;
 	private $pri=101;
 	protected $pro=102;
 
+/* Class methods may be defined as public, private, or protected. Methods declared without any explicit visibility keyword are defined as public.  */
+
+	// function PubFunc(){}
+	
 	public function PubFunc(){
 		echo "Hi I am Public Function";
 	}
