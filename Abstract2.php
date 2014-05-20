5a3cdec0a366c89f1cc9149098e4f68f4cd5fb65
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-21 02:12:29 +1200

    Create Abstract2.php

---
+++ Abstract2.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Abstract classes do not need to be base classes: */
+
+class Foo {
+    public function sneeze() { echo 'achoooo'; }
+}
+
+abstract class Bar extends Foo {
+    public abstract function hiccup();
+}
+
+class Baz extends Bar {
+    public function hiccup() { echo 'hiccup!'; }
+}
+
+$baz = new Baz();
+$baz->sneeze();
+$baz->hiccup();
+
+?>
