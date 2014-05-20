4fbee312dc7fe71cd8c5c1f72ff9f38e819f047f
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-21 07:46:13 +1200

    Create Interface3.php

---
+++ Interface3.php
@@ -0,0 +1,26 @@
+<?php
+
+/* It's possible for interfaces to have constants. Interface constants works exactly like class constants except they cannot be overridden by a class/interface that inherits them.*/
+
+interface a{
+    
+const b = 'Interface constant';
+}
+
+echo a::b;
+
+
+class b implements a{
+
+    //const b = 'Class constant'; // it's not allowed to override constants. (Fatal error: Cannot inherit previously-inherited or override constant b from interface a)
+}
+
+// This works!!!
+class c extends b
+{
+    const b = 'Class constant';
+}
+
+echo c::b;
+
+?>
