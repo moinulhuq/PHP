acbfbbd94fc7fd6a2f386c0761fcd94ed7502033
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2014-05-28 11:49:43 +1200

    Create Singleton.php

---
+++ Singleton.php
@@ -0,0 +1,45 @@
+<?php
+
+/*...........Normal Class.......................*/
+
+class myclass{
+	public function __construct(){
+		
+	}
+}
+
+/*...........Singleton Class.......................*/
+
+class Singleton{
+
+	private function __construct(){
+		//Private constructor
+		//Nobody can instancetiate it
+	}
+	
+	public static function Instance(){
+	
+		static $inst = null;
+		if($inst === null){
+			$inst = new Singleton();
+		}
+		return $inst ;
+	}
+
+}
+
+// $ObjSingleton = new Singleton(); // Fatal error: Call to private Singleton::__construct() from invalid contex
+
+$ObjSingleton1 = Singleton::Instance();
+$ObjSingleton2 = Singleton::Instance();
+var_dump($ObjSingleton1); // object(Singleton)#1 (0) { } 
+var_dump($ObjSingleton2); // object(Singleton)#1 (0) { } 
+
+echo "<br>";
+
+$Objmyclass1 = new myclass();
+$Objmyclass2 = new myclass();
+var_dump($Objmyclass1); // object(myclass)#1 (0) { }
+var_dump($Objmyclass2); // object(myclass)#2 (0) { }  
+
+?>
